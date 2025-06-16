c6b7f199740b35ca9fd70eef31b6e97fd23a906c
Surab Link <surab.l@hotmail.de>
2025-06-16 22:37:01 +0200

    CSS: type background-colors

--- scripts/template.js
+++ scripts/template.js
@@ -1,7 +1,7 @@
 
 function getHTMLListviewCard(pokemonName, pokemonImage, pokemonType) {
     return `
-        <div class="listview_card">
+        <div class="listview_card ${pokemonType}">
             <h2 class="card_title">${pokemonName}</h2>
             <img src="${pokemonImage}" alt="${pokemonName}" class="listview_img">
             <span class="pokemon_type">${pokemonType}</span>
