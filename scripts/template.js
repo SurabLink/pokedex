fe65732798ab39867843eeeea7724a540ec05720
Surab Link <surab.l@hotmail.de>
2025-07-10 21:24:30 +0200

    Template: Capitalize First Letter

--- scripts/template.js
+++ scripts/template.js
@@ -1,9 +1,11 @@
 
 function getHTMLListviewCard(pokémonName, pokémonImage, pokémonType) {
+    let displayPokémonName = pokémonName.charAt(0).toUpperCase() + pokémonName.slice(1);
+    
     return `
         <div class="listview_card ${pokémonType}">
-            <h2 class="card_title">${pokémonName}</h2>
-            <img src="${pokémonImage}" alt="${pokémonName}" class="listview_img">
+            <h2 class="card_title">${displayPokémonName}</h2>
+            <img src="${pokémonImage}" alt="${displayPokémonName}" class="listview_img">
             <span class="pokémon_type">${pokémonType}</span>
         </div>
     `;
